ba0fc582d8be131c4a1ffc62287b9745daa3d7f0
Harold-mont <149728210+Harold-mont@users.noreply.github.com>
2024-06-08 00:00:24 -0300

    FRT - 10.04
    - Solución del despliegue del menú hamburguesa en vista mobile.
    - Modificación de clases en header.

--- frontend/script/myscripts.js
+++ frontend/script/myscripts.js
@@ -1,8 +1,3 @@
-const navToggle = document.querySelector(".nav-toggle-harold");
-const nav = document.querySelector(".nav-harold");
 
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("nav--visible-harold");
-});
 
 
