cabec484f63b693aee6691f6b2add20f62c3f631
DwormiX <jmaguinap@fi.uba.ar>
2024-06-17 17:17:52 -0300

    FRT-? Se agrega el greyout de fechas pasadas en el calendario.

---
+++ frontend/static/js/calendar_disable_past_dates.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("start_date").setAttribute("min", today);
+    document.getElementById("end_date").setAttribute("min", today);
+}
