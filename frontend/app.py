6281f70dd711b7fe6a9a66112bc7ea75bae2f61b
GustavoRiverosFiuba <griveros@fi.uba.ar>
2024-06-18 13:03:04 -0300

    FRT 43
    Ultimos detalles

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
 from datetime import datetime, date
-"""from dotenv import load_dotenv"""
 import os
 import requests
 from flask_cors import CORS
 
-"""load_dotenv()"""
+
 
 
 app = Flask(__name__)
@@ -241,78 +240,3 @@ def reservas():
 
 if __name__ == '__main__':
         app.run(debug=True, port=8080)
-
-"""
-from flask import Flask, render_template, request, jsonify
-from datetime import datetime
-
-app = Flask(__name__)
-
-# Datos simulados
-room_prices = {
-    'single': 50,
-    'double': 80,
-    'suite': 150
-}
-
-reservations = []
-
-@app.route('/')
-def reservar():
-    return render_template('reservar.html')
-
-@app.route('/check_availability', methods=['POST'])
-def check_availability():
-    data = request.get_json()
-    room_type = data['roomType']
-    check_in_date = datetime.strptime(data['checkInDate'], '%Y-%m-%d')
-    check_out_date = datetime.strptime(data['checkOutDate'], '%Y-%m-%d')
-
-    for reservation in reservations:
-        if reservation['roomType'] == room_type:
-            reserved_check_in = datetime.strptime(reservation['checkInDate'], '%Y-%m-%d')
-            reserved_check_out = datetime.strptime(reservation['checkOutDate'], '%Y-%m-%d')
-            if (check_in_date < reserved_check_out) and (check_out_date > reserved_check_in):
-                return jsonify({'available': False})
-
-    return jsonify({'available': True})
-
-@app.route('/make_reservation', methods=['POST'])
-def make_reservation():
-    data = request.get_json()
-    room_type = data['roomType']
-    check_in_date = data['checkInDate']
-    check_out_date = data['checkOutDate']
-
-    total_price = calculate_total_price(room_type, check_in_date, check_out_date)
-    discounted_price = apply_discount(total_price, 0.30)
-
-    reservation = {
-        'roomType': room_type,
-        'checkInDate': check_in_date,
-        'checkOutDate': check_out_date,
-        'totalPrice': total_price,
-        'discountedPrice': discounted_price
-    }
-    reservations.append(reservation)
-
-    return jsonify({
-        'success': True,
-        'totalPrice': total_price,
-        'discountedPrice': discounted_price
-    })
-
-def calculate_total_price(room_type, check_in_date, check_out_date):
-    check_in = datetime.strptime(check_in_date, '%Y-%m-%d')
-    check_out = datetime.strptime(check_out_date, '%Y-%m-%d')
-    days = (check_out - check_in).days
-    price_per_night = room_prices[room_type]
-    return days * price_per_night
-
-def apply_discount(price, discount_rate):
-    return price * (1 - discount_rate)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""    
